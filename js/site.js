ae36ef3074d5bf75f886de59f1573a96609b0f32
Maroswen <marcus.albanese3@gmail.com>
2023-10-17 02:18:30 -0400

    completed requirements

--- js/site.js
+++ js/site.js
@@ -1,13 +1,39 @@
-function displayMessage() {
-    // get the text out of our <input> and assign it to a variable
-    let msg = document.getElementById('message').value;
+function getValues() {
 
-    Swal.fire(
-        {
+    let message = document.getElementById('userInput').value;
+    
+    if (message.length == 0) {
+        Swal.fire({
+            icon: 'error',
             backdrop: false,
-            title: 'APP NAME',
-            text: msg, // <-- replace this text with that variable
-        }
-    );
+            title: 'Oops!',
+            text: 'Please enter a message to see if its a palindrome'
+        });
+    } else {
 
+        let revMsg = checkForPalindrome(message);
+        
+        displayResults(revMsg);
+
+    }
+
+}
+
+function checkForPalindrome(str) { 
+    
+    var a = str.replace(/\s|[0-9_]|\W|[#$%^&*()]/g, "").toLowerCase();
+
+    if (a === a.split("").reverse().join("")) {
+        return true;
+    }
+        return false;
+}
+
+
+
+function displayResults(message) {
+
+    document.getElementById('msg').textContent = `Your message is symmetrical: ${message}!`;
+
+    document.getElementById('alert').classList.remove('invisible');
 }
